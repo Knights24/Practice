d4c520a21dae40183c10c3283a461a216fe44edc
Knights24 <vivekvishwakarma21@outlook.com>
2025-07-13 00:58:11 +0530

    pattern

---
+++ function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int pattern(int n){
+    for(int i=0; i <= n; i++){
+        for(int j=0; j<=i; j++){
+            cout << "*";
+            }
+        cout << endl;
+    }
+}
+
+int main(){
+    cout << pattern(3);
+    return 0;
+}

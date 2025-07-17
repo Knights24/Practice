54cb516cd641a3d74c35dfeb5b1bcbba5e158ea3
Knights24 <vivekvishwakarma21@outlook.com>
2025-07-17 22:55:23 +0530

    setup

--- sort.cpp
+++ sort.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
+ void swap(int &a, int &b){
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+}
+
 int main(){
     int arr[] = {5,4,3,2,1};
-    
+
+    for(int i = 0; i <(sizeof(arr)/sizeof(arr[0]))- 1;i++){
+        
+
+    };
+
 }
